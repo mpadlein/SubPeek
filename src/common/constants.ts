62e3fa0e398af07642cc5467a43822e44a253953
mpadlein <mpadlein@gmail.com>
2026-09-07 11:31:42 +0700

    style: express CACHE_TTL_SECONDS as 60 * 60

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -4,5 +4,4 @@ export const EXTENSION_EVENTS = {
     openOptionsPage: "openOptionsPage",
 };
 
-/** Video cache entries older than this (seconds) are treated as expired. */
-export const CACHE_TTL_SECONDS = 3600;
+export const CACHE_TTL_SECONDS = 60 * 60;
